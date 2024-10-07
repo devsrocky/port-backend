cfebb8cc95f75852accf761e4ea5d15b06366a8d
devsrocky <devsrocky1@gmail.com>
2024-10-07 15:42:16 +0600

    update repository

--- src/service/user/userLoginService.js
+++ src/service/user/userLoginService.js
@@ -5,14 +5,15 @@ const userLoginService = async (Request, DataModel) => {
         let PostBody = Request.body;
         let UserCount = await DataModel.aggregate([
             {$match: PostBody},
-            {$project: { _id: 1, email: 1, mobile: 1, fullName: 1, userPhoto: 1, profileStatus:1}}
+            {$project: { _id: 1, email: 1, mobile: 1, fullName: 1, userPhoto: 1, profileStatus:1, userRole:1}}
             
         ])
         if(UserCount.length> 0){
             if(UserCount[0]['profileStatus'] === 'approved'){
                 // Create token
-                let token = await EncodeToken(UserCount[0]['email'], UserCount[0]['mobile'], UserCount[0]['fullName'], UserCount[0]['userPhoto'], UserCount[0]['_id'])
-                return {status: 'success', token:token,  UserDetails: UserCount[0]}
+                let token = await EncodeToken(UserCount[0]['email'], UserCount[0]['mobile'], UserCount[0]['fullName'], UserCount[0]['userPhoto'], UserCount[0]['userRole'], UserCount[0]['_id'])
+
+                return {status: 'success', token:token}
             }
             return {status: 'pending',  message: 'Before login approve your profile by email verification'}
         }else{
--- src/utility/TokenHelper.js
+++ src/utility/TokenHelper.js
@@ -1,9 +1,9 @@
 const jwt = require('jsonwebtoken')
 
-exports.EncodeToken = (UserEmail, UserMobile, FullName, UserPhoto, user_id) => {
+exports.EncodeToken = (UserEmail, UserMobile, FullName, UserPhoto, userRole, user_id) => {
     let KEY = '1069R-1069O-1069C-1069K-1060-Y-1060-port-folio-BashMuri-rOcky-DevLoper-10Prog'
     let EXPIRE = {expiresIn: '24h'}
-    let PAYLOAD = {UserEmail:UserEmail, UserMobile:UserMobile, FullName:FullName, UserPhoto:UserPhoto, user_id:user_id}
+    let PAYLOAD = {UserEmail:UserEmail, UserMobile:UserMobile, FullName:FullName, UserPhoto:UserPhoto, userRole: userRole, user_id:user_id}
     return jwt.sign(PAYLOAD, KEY, EXPIRE)
 }
 
