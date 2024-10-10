6fb0641295c21acd2a9b26dcb8342bb42ff1e896
devsrocky <devsrocky1@gmail.com>
2024-10-10 10:04:04 +0600

    cors is not working properly

--- app.js
+++ app.js
@@ -21,14 +21,7 @@ const mongoose = require('mongoose')
 
 
 // security lib implement
-
-const corsOptions ={
-   origin:'*', 
-   credentials:true,            //access-control-allow-credentials:true
-   optionSuccessStatus:200,
-}
-
-app.use(cors(corsOptions))
+app.use(cors())
 app.use(helmet())
 app.use(hpp())
 app.use(xss())
